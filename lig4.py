3451c64461d5454e0185e73dca1acf8fdd3baef0
Joao Pedro Soares <73509985+jpsoares01@users.noreply.github.com>
2022-09-20 11:45:05 -0300

    update lig4

--- lig4.py
+++ lig4.py
@@ -1,29 +1,69 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 25 10:53:29 2022
+import numpy as np 
 
-@author: DISRCT
-"""
+linhas = 7
+colunas = 6
+matriz = np.full([colunas,linhas], " ")
 
-matriz =  [['', '', '', '', '', '', ''], 
-           ['', '', '', '', '', '', ''], 
-           ['', '', '', '', '', '', ''], 
-           ['', '', '', '', '', '', ''],
-           ['', '', '', '', '', '', ''],
-           ['', '', '', '', '', '', '']]
+print("\n        bem vindo ao LIG-4")
+print(" -------------------------------------")
+
+jogador1 = "X"
+jogador2 = "O"
 
 
-print("        bem vindo ao LIG-4")
-print("----------------------------------")
+def update_matriz(jogada, player):
+    for i in range(linhas, -1, -1):
+        if matriz[i][jogada] == " ":
+            if player == 0:    
+                player = 1
+                matriz[i][jogada] = jogador1
+                return (print_matriz(matriz))
+            else:
+                player = 0
+                matriz[i][jogada] = jogador2
+                return (print_matriz(matriz))
 
-def print_matriz(matriz):
-        for i in range(len(matriz)):
-            for j in range(len(matriz[i])):
-                print(" |   ", end = '')
-            print("")
+
+def check_column(jogada, player):
+    if matriz[jogada][0] != " ":
+        return False
+    update_matriz(jogada, player)        
 
 
-# def play_game()
+def print_matriz(matriz): 
+    for i in range(colunas):
+        print(f"    {i+1} ", end = '')
+    print("\n -------------------------------------")
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            print(f" |  {matriz[i][j]} ", end = '')
+        print("")
+
 
 print_matriz(matriz)
+
+while 1:
+    player = 1
+    print(f"Vez do jogador: {player}")
+    jogada = int(input("digite a coluna: "))
+    check_column(jogada, player)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     

128ea9bda689d92d2cf80b6755577769e3116730
Joao Pedro Soares <73509985+jpsoares01@users.noreply.github.com>
2022-09-21 23:34:59 -0300

    lig4 with pandas

--- lig4.py
+++ lig4.py
@@ -1,69 +1,55 @@
-import numpy as np 
+import numpy as np
+import pandas as pd
 
-linhas = 7
 colunas = 6
-matriz = np.full([colunas,linhas], " ")
+linhas = 7
+player1 = 'X'
+player2 = 'O'
+player = 1
+matriz = np.full((linhas, colunas), ' ')
 
-print("\n        bem vindo ao LIG-4")
-print(" -------------------------------------")
+print("\n            bem vindo ao LIG-4")
+print(" ------------------------------------------")
 
-jogador1 = "X"
-jogador2 = "O"
-
-
-def update_matriz(jogada, player):
-    for i in range(linhas, -1, -1):
-        if matriz[i][jogada] == " ":
-            if player == 0:    
-                player = 1
-                matriz[i][jogada] = jogador1
-                return (print_matriz(matriz))
-            else:
+def update_matrix(jogada):
+    global player
+    for i in range(linhas-1, -1, -1):
+        if matriz[i][jogada] == ' ':
+            if player == 1:
                 player = 0
-                matriz[i][jogada] = jogador2
-                return (print_matriz(matriz))
+                matriz[i][jogada] = player1
+                break
+            else:
+                player = 1
+                matriz[i][jogada] = player2
+                break
+
+def check_jogada(jogada):
+    if matriz[0][jogada] == ' ':
+        update_matrix(jogada)
+    else:
+        return 1
+def check_win(matriz):
+    df = pd.DataFrame(matriz)
+    
 
 
-def check_column(jogada, player):
-    if matriz[jogada][0] != " ":
-        return False
-    update_matriz(jogada, player)        
-
-
-def print_matriz(matriz): 
+def print_matrix(matriz):
     for i in range(colunas):
-        print(f"    {i+1} ", end = '')
-    print("\n -------------------------------------")
-    for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-            print(f" |  {matriz[i][j]} ", end = '')
-        print("")
+        print(f" |{i+1:^5}", end="")
+    print("|\n ------------------------------------------")
+    for l in range(len(matriz)):
+        for c in range(len(matriz[l])):
+            print(f" |{matriz[l][c]:^5}", end='')
+        print("|")
+    print(" ------------------------------------------")
 
-
-print_matriz(matriz)
-
-while 1:
-    player = 1
-    print(f"Vez do jogador: {player}")
-    jogada = int(input("digite a coluna: "))
-    check_column(jogada, player)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+print_matrix(matriz)
+check_win(matriz)
+# while 1:
+#     print(f"Vez do jogador: {player}")
+#     jogada = int(input("Digite qual a coluna que deseja jogar: "))-1
+#     if check_jogada(jogada) == 1:
+#         print("Jogada invalida, tente jogar em outra coluna...")
+#     print_matrix(matriz)
     
